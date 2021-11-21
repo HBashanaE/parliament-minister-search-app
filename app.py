33e3c09c1e2ae6e0a36831bd01ff3f1bf2ac220e
Bashana Elikewela <hbashanae@gmail.com>
2021-11-21 22:25:49 +0530

    ackendd error handling

--- app.py
+++ app.py
@@ -6,18 +6,23 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')    
+    return render_template('index.html')
 
 @app.route('/search', methods=['GET'])
 def search():
     query  = request.args.get("query")
+    # print(query)
     intents, tokenized_query, query = classify_intent(query)
+    # print(intents, tokenized_query, query)
     body = build_query(query, tokenized_query, intents)
-    print('Body: ', body)
-    res = es.search(index="parliament", body=body)
-    data = res['hits']['hits']
-    # print(json.dumps(res, indent=4, ensure_ascii=False))
-    return jsonify([i['_source'] for i in data])
+    # print('Body: ', body)
+    try:
+        res = es.search(index="parliament", body=body)
+        data = res['hits']['hits']
+        # print(json.dumps(res, indent=4, ensure_ascii=False))
+        return jsonify([i['_source'] for i in data])
+    except:
+        return 'Backend Error', 400
 
 if __name__ == '__main__':
     app.run(debug=True)
